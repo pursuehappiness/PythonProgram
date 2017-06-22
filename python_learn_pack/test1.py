7ddad989967dcb9732d8cdd5c80d421242b7abe4
lucient <pursueexcel@gmail.com>
2017-06-22 21:38:50 +0800

    Try print/input function and string format

--- python_learn_pack/test1.py
+++ python_learn_pack/test1.py
@@ -1,3 +1,13 @@
 
 def returnfunc(str):
 	return str
+
+
+
+
+if __name__ == '__main__':
+	print('start learn python')
+	print('test input func')
+	print('please input a name')
+	name = input()
+	print('you name is \'%s\'' % name)

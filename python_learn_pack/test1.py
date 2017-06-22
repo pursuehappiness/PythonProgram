f9830363649a70b51240e48c57b1d8201b0b985f
lucient <pursueexcel@gmail.com>
2017-06-22 22:12:58 +0800

    Learn list properties

--- python_learn_pack/test1.py
+++ python_learn_pack/test1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 def returnfunc(str):
 	return str
@@ -5,7 +7,35 @@ def returnfunc(str):
 def floordiv(a,b):
 	return a//b
 
+def testlist():
+	print('start test list')
+	classmates = ['Michael', 'Bob', 'Tracy']
+	print('classmates list lenght is',len(classmates))
+	print('classmates[0]=',classmates[0])
+	print('the last classmate is',classmates[-1])
+	print('append admin to calssmate')
+	classmates.append('admin')
+	print(classmates)
+	print('delete last element')
+	classmates.pop()
+	print(classmates)
+	print('delete the second element')
+	classmates.pop(1)
+	print(classmates)
+	print('replace second element with sarah')
+	classmates[1] = 'sarah'
+	print(classmates)
 
+	print('\n======================\n')
+	print('list can store diff type of element')
+	l=['apple',12,True]
+	print(l)
+
+	print('list can sotre list')
+	s = ['asp','python','c#',l]
+	print(s)
+	print('len(s) = ',len(s))
+	print('len(s[3])=',len(s[3]))
 
 if __name__ == '__main__':
 	print('start learn python')
@@ -21,3 +51,5 @@ if __name__ == '__main__':
 	a = 'XYZ'
 	print('a =',a)
 	print('b =',b)
+
+	testlist()
